4e89eeb5d4c38660f057689998f3c643320bcaf8
David Igbo <davidokpah396@gmail.com>
2025-06-30 16:27:55 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,10 @@
 Rails.application.routes.draw do
-  get "bookings/new"
-  get "bookings/create"
-  get "bookings/index"
-  get "events/index"
-  get "events/new"
-  get "events/create"
-  get "events/edit"
-  get "events/update"
-  get "events/destroy"
-  get "events/show"
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'event#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'login', to: 'session#inew'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :events
+  resources :bookings, only: [:index, :new, :create]
 end
