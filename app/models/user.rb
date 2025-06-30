994d66bc736c7f4f65717358088d95cb5a881d94
David Igbo <davidokpah396@gmail.com>
2025-06-30 15:04:49 +0100

    add models relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    enum role: { customer:0, organizer: 1 }
+
+    has_many evets, dependent: :destroy
+    has_many bookings, dependent: :destroy
 end
